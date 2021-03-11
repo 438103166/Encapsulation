3629b3323123c5b3c26a48614b4d485f1c2649b7
DELL <DELL@DESKTOP-IF2PEG5>
2021-03-11 11:27:46 +0300

    First Commit

---
+++ Car2/src/Car.java
@@ -0,0 +1,25 @@
+
+ class Car {
+	private String name;
+	private double topSpead;
+
+	Car() {
+		
+	}
+public String getName() {
+	return name;
+	}
+
+public void setName(String Num) {
+	
+}
+public void setTopSpead(double Spead) {
+	
+}
+public double getTopSpeadMPH() {
+	return topSpead;
+}
+public double getTopSpeadKMH() {
+	return topSpead;
+}
+}
---
+++ Car2/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car A=new Car();
+		A.setName("Hondai");
+		A.getName();
+		System.out.println(A.getName());
+		A.setTopSpead(40.6);
+		A.getTopSpeadMPH();
+		A.getTopSpeadKMH();
+		System.out.println(A.getTopSpeadMPH());
+		System.out.println(A.getTopSpeadKMH());
+		
+
+	}
+
+}
